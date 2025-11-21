98df938dd280629326974ccd71cd00044994b653
Gabriel <gabriel.rodrigues.costa2001@gmail.com>
2025-11-21 14:57:46 -0300

    fix(bugs): Corrigir bugs

--- src/server.js
+++ src/server.js
@@ -6,15 +6,19 @@ import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 4000; 
+const PORT = process.env.PORT || 4000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+// rota para evitar Bad Gateway
+app.get("/", (req, res) => {
+  res.send("API funcionando 🚀");
+});
+
 connectDB();
 
 app.use("/api", userRoutes);
-
 
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
